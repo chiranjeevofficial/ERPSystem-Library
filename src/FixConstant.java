6a85b08720655da8d3d89825483590f648dc1568
chiranjeevofficial <chiranjeevkashyap060@gmail.com>
2023-02-28 20:05:41 +0530

    commit

---
+++ src/FixConstant.java
@@ -0,0 +1,6 @@
+public class FixConstant {
+    public static int textFieldWidth = 200;
+    public static int textFieldX = 150;
+    public static int labelWidth = 150;
+    public static int buttonWidth = 90;
+}
