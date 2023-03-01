b59080a5cfa09e561aa75f42f10c700188e2991e
chiranjeevofficial <chiranjeevkashyap060@gmail.com>
2023-03-01 08:19:10 +0530

    update

--- src/FixConstant.java
+++ src/FixConstant.java
@@ -1,6 +1,9 @@
 public class FixConstant {
+    public static int labelMarginX = 20;
+    public static int labelHeight = 30;
+    public static int labelWidth = 120;
+
+    public static int textFieldMarginX = 150;
     public static int textFieldWidth = 200;
-    public static int textFieldX = 150;
-    public static int labelWidth = 150;
-    public static int buttonWidth = 90;
+    public static int textFieldHeight = 30;
 }
