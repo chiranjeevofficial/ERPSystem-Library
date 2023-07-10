312954d09ebad4e64a82da0765e4a8c0de356739
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-07-11 00:18:11 +0530

    update

--- InventryManagementSystem/src/inventrymanagementsystem/Main.java
+++
@@ -1,9 +0,0 @@
-package inventrymanagementsystem;
-
-public class Main {
-    public static void main(String[] args) {
-        new Login();
-//        JTable table = new JTable();
-//        JScrollPane jScrollPane = new JScrollPane();
-    }
-}
---
+++ src/lms/Main.java
@@ -0,0 +1,7 @@
+package lms;
+
+public class Main {
+    public static void main(String[] args) {
+        new HomePage();
+    }
+}
